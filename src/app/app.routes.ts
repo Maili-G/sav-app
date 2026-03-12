e217774ffdd7bcbe3c32d5bc1e6b431c8d428cf2
Nai <pitaya@gmail.com>
2026-03-12 11:59:45 +0100

    ajout ingrédient et calculatrice

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,7 +4,7 @@ import { LoginPage } from './pages/login-page/login-page';
 import { SubscribePage } from './pages/subscribe-page/subscribe-page';
 import { AccountManagerPage } from './pages/account-manager-page/account-manager-page';
 import { RecipeCalculatorPage } from './pages/recipe-calculator-page/recipe-calculator-page';
-import { RecipeManagerPage } from './pages/recipe-manager-page/recipe-manager-page';
+import { RecettesManagerPage } from './pages/recipe-manager-page/recipe-manager-page';
 import { UsersManagerPage } from './pages/users-manager-page/users-manager-page';
 import { IngredientsManagerPage } from './pages/ingredients-manager-page/ingredients-manager-page';
 import { AboutPage } from './pages/about-page/about-page';
@@ -24,7 +24,7 @@ export const routes: Routes = [
 
     // Recettes :
     { path: 'recipe-calculator', component: RecipeCalculatorPage },
-    { path: 'recipe-manager', component: RecipeManagerPage},
+    { path: 'recipe-manager', component: RecettesManagerPage},
 
     // Administration - Gestion :
     { path: 'users-manager', component: UsersManagerPage },
--- src/app/services/ingredient.service.ts
+++ src/app/services/ingredient.service.ts
@@ -29,10 +29,27 @@ export class IngredientService {
   getIngredientById(id: number): Observable<Ingredient> {
     return this.http.get<Ingredient>(`${this.API_URL_INGREDIENT}/${id}`);
   }
-  
-  // TODO : Créer plus tard les méthodes manquantes :
-  //    - deleteIngredients(id: number)
-  //    - deleteAllIngredients()
-  //    - addIngredient(ingredient: Ingredient)
-  //    - updateIngredient(id: number, ingredient: Ingredient)
+
+  // ... (code précédent)
+  /** * Ajoute un nouvel ingrédient.
+  */
+  addIngredient(ingredient: Ingredient): Observable<Ingredient> {
+    return this.http.post<Ingredient>(this.API_URL_INGREDIENT, ingredient);
+  }
+  /** * Met à jour un ingrédient existant.
+  */
+  updateIngredient(ingredient: Ingredient): Observable<Ingredient> {
+    return this.http.put<Ingredient>
+      (`${this.API_URL_INGREDIENT}/${ingredient.id}`, ingredient);
+  }
+  /** * Supprime un ingrédient par son ID.
+  */
+  deleteIngredient(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL_INGREDIENT}/${id}`);
+  }
+  /** * Supprime tous les ingrédients de la base.
+  */
+  deleteAllIngredients(): Observable<void> {
+    return this.http.delete<void>(`${this.API_URL_INGREDIENT}/all`);
+  }
 }
