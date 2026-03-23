4cc2703b9b33d5fa7d525f5a8e945d1cccaf7c7f
Nai <pitaya@gmail.com>
2026-03-23 12:00:25 +0100

    page d'inscription

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -11,26 +11,69 @@ export class AuthService {
   private router = inject(Router);
 
   // Signal pour suivre l'état de la connexion :
-  public isAuthenticated = signal<boolean>(this.hasToken());
+  // public isAuthenticated = signal<boolean>(this.hasToken());
   constructor(private http: HttpClient) { }
+
+  // Méthode d'inscription
+  subscribe(credentials: any): Observable<any> {
+    return this.http.post(`${this.API_URL}/register`, credentials);
+  }
+
   login(credential: any): Observable<any> {
     return this.http.post(`${this.API_URL}/login`, credential).pipe(
       tap((response: any) => {
         if (response.token) {
           localStorage.setItem(this.TOKEN_KEY, response.token);
-          this.isAuthenticated.set(true);
+          // this.isAuthenticated.set(true);
         }
       })
     )
   }
   logout(): void {
     localStorage.removeItem(this.TOKEN_KEY);
-    this.isAuthenticated.set(false);
+    // this.isAuthenticated.set(false);
+    this.router.navigate(['/login']);
   }
   getToken(): string | null {
     return localStorage.getItem(this.TOKEN_KEY);
   }
   private hasToken(): boolean {
     return !!localStorage.getItem(this.TOKEN_KEY);
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+
+  private getDecodedToken(): any {
+    const token = this.getToken();
+    if (!token) return null;
+    try {
+      const payload = token.split('.')[1];
+      return JSON.parse(atob(payload));
+    } catch (e) {
+      return null;
+    }
+  }
+
+  getUserIdentifier(): string {
+    const decoded = this.getDecodedToken();
+    return decoded ? decoded.sub : 'Invité';
+  }
+
+  hasRole(role: string): boolean {
+    const decoded = this.getDecodedToken();
+    if (!decoded || decoded.role) return false;
+    return decoded.roles.includes(role);
+  }
+
+  getUserFullInfo() {
+    const decoded = this.getDecodedToken();
+    if (!decoded) return null;
+    return {
+      username: decoded.sub,
+      roles: decoded.role || [],
+      expiration: new Date(decoded.exp * 1000)
+    };
   }
 }
